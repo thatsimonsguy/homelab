cb1d720a831c235d93371963a02ad9c901c0ada7
Matt Simons <thatsimonsguy@gmail.com>
2024-07-17 22:21:38 -0500

    dude, fuck yq

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,21 +31,7 @@ git clone https://github.com/thatsimonsguy/homelab.git /home/oebus/homelab
 # Navigate to the ansible directory
 cd /home/oebus/homelab/ansible
 
-# Install yq for YAML processing (Mike Farah version)
-sudo wget https://github.com/mikefarah/yq/releases/download/v4.9.8/yq_linux_amd64 -O /usr/local/bin/yq && sudo chmod +x /usr/local/bin/yq
-
-# Add yq to the PATH
-export PATH=$PATH:/usr/local/bin
-
-# Check if ubuntu_release exists in config.yml and add it if not
-if ! yq eval '.ubuntu_release' config.yml &>/dev/null; then
-  yq eval --inplace ".ubuntu_release = \"$UBUNTU_RELEASE\"" config.yml
-  echo "Added ubuntu_release to config.yml"
-else
-  echo "ubuntu_release already exists in config.yml"
-fi
-
 # Run the Ansible playbook with the inventory file
-ansible-playbook -i inventory.ini bootstrap_playbook.yml --vault-password-file <(echo "$1")
+ansible-playbook -i inventory.ini bootstrap_playbook.yml --vault-password-file <(echo "$1") --extra-vars "ubuntu_release=$UBUNTU_RELEASE"
 
 echo "Bootstrapping complete."
