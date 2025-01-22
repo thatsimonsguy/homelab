78d2ee7efb487836441dfeeb2f54f58dbd26f241
Matt Simons <thatsimonsguy@gmail.com>
2025-01-22 14:21:25 -0600

    removing more references to ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,31 +7,45 @@ if [ -z "$1" ]; then
 fi
 
 # Update the system
+echo "Updating system packages..."
 sudo apt update && sudo apt upgrade -y
 
 # Install essential packages
-sudo apt install -y curl software-properties-common git
+echo "Installing essential packages..."
+sudo apt install -y curl gnupg git software-properties-common python3 python3-pip
 
-# Add ansible PPA and install ansible
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
+# Install Ansible using pip (preferred for Debian to avoid PPA issues)
+echo "Installing Ansible..."
+sudo pip3 install ansible
 
-# Verify ansible installation
+# Verify Ansible installation
 ansible --version
+if [ $? -ne 0 ]; then
+  echo "Ansible installation failed. Exiting..."
+  exit 1
+fi
 
-echo "Ansible & Git installed."
+echo "Ansible & Git installed successfully."
 
-# Detect the Ubuntu release codename
-UBUNTU_RELEASE=$(lsb_release -cs)
-export UBUNTU_RELEASE
+# Detect the Debian codename
+DEBIAN_RELEASE=$(grep "VERSION_CODENAME" /etc/os-release | cut -d'=' -f2)
+export DEBIAN_RELEASE
 
 # Clone the homelab repository
-git clone https://github.com/thatsimonsguy/homelab.git /home/oebus/homelab
+HOMELAB_DIR="/home/oebus/homelab"
+if [ -d "$HOMELAB_DIR" ]; then
+  echo "Homelab directory already exists. Pulling latest changes..."
+  git -C "$HOMELAB_DIR" pull
+else
+  echo "Cloning homelab repository..."
+  git clone https://github.com/thatsimonsguy/homelab.git "$HOMELAB_DIR"
+fi
 
 # Navigate to the ansible directory
-cd /home/oebus/homelab/ansible
+cd "$HOMELAB_DIR/ansible" || { echo "Failed to navigate to ansible directory. Exiting..."; exit 1; }
 
 # Run the Ansible playbook with the inventory file
-ansible-playbook -i inventory.ini bootstrap.yml --vault-password-file <(echo "$1") --extra-vars "ubuntu_release=$UBUNTU_RELEASE"
+echo "Running Ansible playbook..."
+ansible-playbook -i inventory.ini bootstrap.yml --vault-password-file <(echo "$1") --extra-vars "debian_release=$DEBIAN_RELEASE"
 
 echo "Bootstrapping complete."
