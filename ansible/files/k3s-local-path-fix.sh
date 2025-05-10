d07d92a1b080628b98c8ee9d4a0cdfbb44e969e3
Matt Simons <thatsimonsguy@gmail.com>
2025-05-10 18:20:44 -0500

    replacing whole block in configmap

--- ansible/files/k3s-local-path-fix.sh
+++ ansible/files/k3s-local-path-fix.sh
@@ -3,10 +3,11 @@
 echo "Waiting for K3s API to be available..."
 until kubectl get nodes &>/dev/null; do sleep 1; done
 
-echo "Patching local-path-config..."
-kubectl -n kube-system patch configmap local-path-config \
-  --type merge \
-  --patch-file /usr/local/share/local-path-patch.json
+echo "Replacing local-path-config configmap..."
+kubectl get configmap local-path-config -n kube-system -o json | \
+  jq --slurpfile patch /usr/local/share/local-path-patch.json \
+     '.data["config.json"] = ($patch[0].data["config.json"])' | \
+  kubectl replace -f -
 
 echo "Restarting local-path-provisioner..."
 kubectl -n kube-system delete pod -l app=local-path-provisioner
