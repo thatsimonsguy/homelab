66fbf88068c4781e8146aa029b74ae312eba70ee
Matt Simons <thatsimonsguy@gmail.com>
2025-05-10 18:07:20 -0500

    json in bash string escaping hell

--- ansible/files/k3s-local-path-fix.sh
+++ ansible/files/k3s-local-path-fix.sh
@@ -6,14 +6,7 @@ until kubectl get nodes &>/dev/null; do sleep 1; done
 echo "Patching local-path-config..."
 kubectl -n kube-system patch configmap local-path-config \
   --type merge \
-  --patch "$(cat <<'EOF'
-{
-  "data": {
-    "config.json": "{ \"nodePathMap\": [ { \"node\": \"DEFAULT_PATH_FOR_NON_LISTED_NODES\", \"paths\": [\"/var/lib/rancher/k3s/storage\"] }, { \"node\": \"burdturglar\", \"paths\": [\"/mnt/nas/k8sconfig\"] } ] }"
-  }
-}
-EOF
-)"
+  --patch-file /usr/local/share/local-path-patch.json
 
 echo "Restarting local-path-provisioner..."
 kubectl -n kube-system delete pod -l app=local-path-provisioner
